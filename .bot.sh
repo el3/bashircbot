194e6aab3d808c0486b2bed711a8d32c18cf7cc5
el3 <bogisivertsen@gmail.com>
2016-01-11 02:46:16 +0100

    Update .bot.sh

--- .bot.sh
+++ .bot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-. .bot.properties
-input=".bot.cfg"
+. .bot.config
+input=".bot.output"
 echo "NICK $nick" > $input 
 echo "USER $user" >> $input
 joined="false"
@@ -12,10 +12,12 @@ do
   case "$res" in
     # respond to ping requests from the server
     PING*)
-      echo "$res" | sed "s/I/O/" >> $input 
+      echo "$res" | sed "s/I/O/" >> $input
+      echo "$res"
     ;;
 
-   *"001 $nick"*)
+   *"001 $nick"*) # The first message sent after client registration. The text used varies widely https://www.alien.net.au/irc/irc2numerics.html
+      echo "$res"
       if [ "$joined" = "false" ]
         then
           for i in "${channels[@]}"
@@ -31,24 +33,32 @@ do
     *PRIVMSG*)
 
       echo "$res"
-      from=$(echo "$res" | perl -pe "s/.*PRIVMSG (.*[#]?([a-zA-Z]|\-)*) :.*/\1/")
-      cmd=$(echo "$res" | perl -pe "s/^.*?PRIVMSG\s#.*?:#\s(.*?)\r/\1/")
-      prefix=$(echo "$res" | perl -pe "s/^.*?PRIVMSG\s#.*?:(#)\s.*/\1/")
-      cmd="$cmd 2>&1 | sed 's/.\/.bot.sh: line 43: //'"
-      i="0"
       
-      if [ "$prefix" = '#' ]
+      # If you need to know who sent the command for some reason
+      from=$(echo "$res" | perl -pe "s/.*PRIVMSG (.*[#]?([a-zA-Z]|\-)*) :.*/\1/")
+      
+      # the command itself
+      cmd=$(echo "$res" | perl -pe "s/^.*?PRIVMSG\s#.*?:#\s(.*?)\r/\1/")
+      
+      # Extract the prefix from  the msg. Example if you send "# ls -lah" # is the prefix
+      prefix=$(echo "$res" | perl -pe "s/^.*?PRIVMSG\s#.*?:(.)\s.*/\1/")
+      
+      # Delete the script reference from the output when command fails.
+      # Needs to be the line number where the command is evaluated -> eval $cmd > .output is
+      cmd="$cmd 2>&1 | sed 's/.\/.bot.sh: line 53: //'"
+      i="0"  # a counter if you want to max the number of outputted lines from one command
+      
+      if [ "$prefix" = "$cmdprefix" ] # if the prefix matches
       then
-        echo 1 >> .output.txt
-        eval $cmd > .output.txt
-        while read p; do
-            if [ $i -lt 10 ]
+        eval $cmd > .output  # this is the line where the command is evaluated. And the output is written to .output
+        while read p; do     # for each line in .output
+            if [ $i -lt $maxlines ] # If you have set the maxlines to 10 in .bot.config. 
               then
-              echo "PRIVMSG $from :"$p >> $input
-              sleep 0.8
+              echo "PRIVMSG $from :"$p >> $input  # then it will print the 10 first lines of .output
+              sleep 0.8                           # sleep time, or else it will disconnect when to many lines
             fi
-            i=$[$i+1]
-        done <.output.txt
+            i=$[$i+1] # increment counter
+        done <.output        # .output is passed to the while loop
       fi
 
     ;;
