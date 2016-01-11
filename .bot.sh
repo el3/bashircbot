247cc3d395acbb8533242dae205a61dcb5a3fa3f
el3 <bogisivertsen@gmail.com>
2016-01-11 07:34:00 +0100

    Update .bot.sh

--- .bot.sh
+++ .bot.sh
@@ -34,32 +34,7 @@ do
 
       echo "$res"
       
-      # If you need to know who sent the command for some reason
-      from=$(echo "$res" | perl -pe "s/.*PRIVMSG (.*[#]?([a-zA-Z]|\-)*) :.*/\1/")
-      
-      # the command itself
-      cmd=$(echo "$res" | perl -pe "s/^.*?PRIVMSG\s#.*?:#\s(.*?)\r/\1/")
-      
-      # Extract the prefix from  the msg. Example if you send "# ls -lah" # is the prefix
-      prefix=$(echo "$res" | perl -pe "s/^.*?PRIVMSG\s#.*?:(.)\s.*/\1/")
-      
-      # Delete the script reference from the output when command fails.
-      # Needs to be the line number where the command is evaluated -> eval $cmd > .output is
-      cmd="$cmd 2>&1 | sed 's/.\/.bot.sh: line 53: //'"
-      i="0"  # a counter if you want to max the number of outputted lines from one command
-      
-      if [ "$prefix" = "$cmdprefix" ] # if the prefix matches
-      then
-        eval $cmd > .output  # this is the line where the command is evaluated. And the output is written to .output
-        while read p; do     # for each line in .output
-            if [ $i -lt $maxlines ] # If you have set the maxlines to 10 in .bot.config. 
-              then
-              echo "PRIVMSG $from :"$p >> $input  # then it will print the 10 first lines of .output
-              sleep 0.8                           # sleep time, or else it will disconnect when to many lines
-            fi
-            i=$[$i+1] # increment counter
-        done <.output        # .output is passed to the while loop
-      fi
+      ./.rec.sh "$res"
 
     ;;
 
