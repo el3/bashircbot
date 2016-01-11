fd11c1eb689a6632d76b1eb60bdd6dd31adc0bda
el3 <bogisivertsen@gmail.com>
2016-01-11 07:57:46 +0100

    create screen sessions

--- .scr.sh
+++ .scr.sh
@@ -2,6 +2,9 @@
 
 session=$(screen -list | awk '{print $1}' | grep "^[0-9]*.$1$")
 
+# I was experimenting with commands that would crash the bot like nano, tail -f, more, less, etc..
+# Create a screen session for each user
+# This might not be neccesary
 if [ "$session" = "" ] ; then
     screen -d -m -S $1
     session=$(screen -list | awk '{print $1}' | grep "^[0-9]*.$1$")
