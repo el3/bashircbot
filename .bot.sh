2a604e9814647dd1838eb68ff3f1f38274739956
el3 <bogisivertsen@gmail.com>
2016-02-02 11:48:23 +0100

    Update .bot.sh

--- .bot.sh
+++ .bot.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# This is a irc bot, wrote by el3. I can be found on freenode and spotchat
-# Be aware if you run this on your server. Users can run nasty commands like # sudo rm -rf /*
-
 . .bot.config
 input=".bot.output"
 echo "NICK $nick" > $input 
@@ -37,8 +34,31 @@ do
 
       echo "$res"
       
-      ./.rec.sh "$res"  # pass the msg to .rec.sh
-
+      # the channel the command was sent from
+      channel=$(echo "$res" | perl -pe "s/.*PRIVMSG (.*[#]?([a-zA-Z]|\-)*) :.*/\1/")
+      
+      # the command itself
+      cmd=$(echo "$res" | awk 'BEGIN { FS = "^.*?PRIVMSG.*?:[#$]|[\r\n]" } {print $2}') # [$#]
+      
+      # Who sent the command
+      from=$(echo "$res" | perl -pe "s/^:(.*?)!.*/\1/")
+      
+      i="0"  # a counter if you want to max the number of outputted lines from one command
+      
+      if [ "$cmd" != ""  ]
+          then
+          cmd="$cmd 2>&1 | sed 's/.bot.sh: line 51: //'"  # remove the script reference from error
+          eval "$cmd" > .output
+          while read p; do     # for each line in .output
+              if [ $i -lt $maxlines ]  # If you have set the maxlines to 10 in .bot.config. 
+                then
+                echo "PRIVMSG $channel :$p" >> $input  # then it will print the 10 first lines of .output
+                sleep 0.1                         # sleep time, or else it will disconnect when to many lines
+              fi
+              i=$[$i+1] # increment counter
+          done <.output        # .output is passed to the while loop
+          rm .output
+      fi
     ;;
 
     *)
